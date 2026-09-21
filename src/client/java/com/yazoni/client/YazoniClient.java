12090c8b0df3de8c96e24387d46ba71c234a354d
yazoniplay <yazan.mahmoud2@outlook.com>
2026-09-21 18:56:01 +0200

    Wire built-in Yazoni module registry into client startup

--- src/client/java/com/yazoni/client/YazoniClient.java
+++ src/client/java/com/yazoni/client/YazoniClient.java
@@ -1,5 +1,7 @@
 package com.yazoni.client;
 
+import com.yazoni.client.module.BuiltinModules;
+import com.yazoni.client.module.ModuleManager;
 import net.fabricmc.api.ClientModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,9 +9,11 @@ import org.slf4j.LoggerFactory;
 public final class YazoniClient implements ClientModInitializer {
     public static final String MOD_ID = "yazoni";
     public static final Logger LOGGER = LoggerFactory.getLogger("Yazoni Client");
+    public static final ModuleManager MODULES = new ModuleManager();
 
     @Override
     public void onInitializeClient() {
-        LOGGER.info("Yazoni Client foundation initialized.");
+        BuiltinModules.registerAll(MODULES);
+        LOGGER.info("Yazoni Client initialized with {} built-in modules.", MODULES.all().size());
     }
 }
